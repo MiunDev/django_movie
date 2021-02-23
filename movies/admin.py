37a3dca9a59a8a0f1d12dbe673661ce5410a4f23
Marsel <fatkullinmars23@gmail.com>
2021-02-23 22:33:28 +0500

    Настроена админка и добавлен редактор для description

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+
+# для поля description добавили онлайн редактор текста
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 # конфигурирование наших записей в админке
@@ -9,9 +21,22 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ("name",)
 
 
+# отображение всех отзывов к выбранному фильму
 class ReviewInline(admin.TabularInline):
     model = Reviews
+    extra = 1
     readonly_fields = ("name", "email")
+
+
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
+
+    get_image.short_description = "Изображение"
 
 
 @admin.register(Movie)
@@ -19,17 +44,78 @@ class MovieAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
     search_fields = ("title", "category__name")
+    inlines = [MovieShotsInline, ReviewInline]
+    readonly_fields = ("get_image",)
+    save_on_top = True  # кнопка сохранить отображается также и сверху
+    save_as = True  # при изменении фильма, можно сохранить как новый объект с сохраненными полями
+    list_editable = ("draft",)  # редактирование поля прямо в списке фильмов (черновик)
+    form = MovieAdminForm
+    # fields = (("actors", "directors", "genres"),)  # позволяет скрыть ненужные поля не указанные в этом кортедже
+    # группировка полей:
+    fieldsets = (
+        (None, {
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": ("description", "poster", "get_image",)
+        }),
+        (None, {
+            "fields": (("year", "world_premiere", "country"),)
+        }),
+        ("Actors", {
+            'classes': ('collapse',),
+            "fields": ("actors", "directors", "genres", "category",)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fess_in_world"),)
+        }),
+        ("Options", {
+            "fields": (("url", "draft"),)
+        }),
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
+
+    get_image.short_description = "Постер"
 
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "parent", "movie", "id")
     readonly_fields = ("name", "email")
-    inlines = [ReviewInline]
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Актеры"""
+    list_display = ("name", "age", "get_image")
+    readonly_fields = ("get_image",)
+
+    # вывод мини изображения напротив актера в списке
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = "Изображение"
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Актеры"""
+    list_display = ("title", "movie", "get_image")
+    readonly_fields = ("get_image",)
+
+    # вывод мини изображения напротив актера в списке
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = "Изображение"
 
 
 admin.site.register(Genre)
-admin.site.register(MovieShots)
-admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
+
+# заголовок сайта в админке
+admin.site.site_title = "Django Movies"
+admin.site.site_header = "Django Movies"
--- movies/apps.py
+++ movies/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class MoviesConfig(AppConfig):
     name = 'movies'
+    verbose_name = "Фильмы"  # наименование категории в админке, также смотреть файл __init__.py
